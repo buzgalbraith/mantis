d1c8e697613d28f8b811da582576c063cdfd0b46
buz galbraith <buzgalbraith@gmail.com>
2026-03-04 11:21:05 -0500

    Added code for pulling data from synapse

---
+++ scripts/pull_data_from_synapse.py
@@ -0,0 +1,21 @@
+"""
+Downloads a list of FastQ files from synapse to ./data
+Note: Must sign up for an account on synapse and set your auth token to the environmental variable `$SYNAPSE_AUTH_TOKEN`
+"""
+import csv
+import os 
+import synapseclient
+
+def main():
+    """pull files from synapse"""
+    with open('synapse_data/fastq_file_list.tsv', mode = 'r') as f:
+        reader = csv.reader(f, delimiter = '\t')
+        for _, fid, _ in reader:
+            syn.get(fid, downloadLocation = './synapse_data', ifcollision='keep.local')
+            break
+
+if __name__ == "__main__":
+    syn = synapseclient.login()
+    assert syn._is_logged_in()
+    main()
+
