590d4c6f790961760bf5880a91090da3da68352c
buz galbraith <buzgalbraith@gmail.com>
2026-03-18 18:07:34 -0400

    Added target number for download

--- scripts/pull_data_from_synapse.py
+++ scripts/pull_data_from_synapse.py
@@ -6,16 +6,20 @@ import csv
 import os 
 import synapseclient
 
-def main():
+def main(target_num:int = 750):
     """pull files from synapse"""
+    i = 0
     with open('synapse_data/fastq_file_list.tsv', mode = 'r') as f:
         reader = csv.reader(f, delimiter = '\t')
         for _, fid, _ in reader:
             syn.get(fid, downloadLocation = './synapse_data', ifcollision='keep.local')
-            break
+            i+= 1
+            if i >= target_num:
+                break
 
 if __name__ == "__main__":
+    num_files = 750
     syn = synapseclient.login()
     assert syn._is_logged_in()
-    main()
+    main(num_files)
 
